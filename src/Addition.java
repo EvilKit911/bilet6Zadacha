f5365cc4fccfa91913507f14d85f70310c479119
Nikita <19nikita98.3939@gmail.com>
2022-12-06 20:27:23 +0200

    Исправления

--- src/Addition.java
+++ src/Addition.java
@@ -1,23 +1,26 @@
 public class Addition {
     public Addition(int[] random) {
-            for (int i = 0; i < random.length; i++) {
+        for (int i = 0; i < random.length; i++) {
 
-                System.out.println( "№ "+ i +"  "+ random[i]);// распечатал весь массив чисел
-            }
+            System.out.println("№ " + i + "  " + random[i]);// распечатал весь массив чисел
+        }
         System.out.println(" ");
 
         for (int i = 0; i < random.length; i++) {
-            if (random[i] % 2 != 0 ) {
+            if (random[i] % 2 != 0) {
                 random[i] += 1;
             }
-            System.out.println( "№ "+ i +"  "+ random[i]) ;
+        }
+        for (int i = 0; i < random.length; i++) {
+            if (i > 1 && i < 8) {
+                System.out.println("№ " + i + "  " + random[i]);
+            }
         }
 
 
-
     }
+}
 
-    }
 
 
 
